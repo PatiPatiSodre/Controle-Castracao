5a54b9381f4473d8d2b78828971d0cafa17c5f0b
Patrícia Sodré <110927380+PatiPatiSodre@users.noreply.github.com>
2024-08-13 13:36:21 -0300

    Update Altera-Animal.php

--- Animal/Altera-Animal.php
+++ Animal/Altera-Animal.php
@@ -12,8 +12,8 @@ $row = mysqli_fetch_array($result);
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/Style.css" />
-    <link rel="stylesheet" href="../css/Cadastro.css" />
+    <link rel="stylesheet" href="../CSS/Style.css" />
+    <link rel="stylesheet" href="../CSS/Cadastro.css" />
     <title>Altera Animal</title>
 </head>
 
